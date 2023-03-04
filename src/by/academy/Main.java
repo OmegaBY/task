a40093a4d52b2083ecca5e530af1edd8c0bdb51c
Aliaksei Samborskiy <samborskiyalex@gmail.com>
2023-03-04 06:44:41 +0300

    00 - lesson - 3 - math + inc and dec( ++i++)

--- src/by/academy/Main.java
+++ src/by/academy/Main.java
@@ -4,5 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Lesson 3");
+
+        int i = 1;
+        int m = 2* ++i + i++;
+        System.out.println(m);
+        System.out.println(i);
+
+
+
+
+
+        /*
+        int x = 6;
+
+        if (x > 5 || 1 < x && x < 5) { // если я правильно понял
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+
+        };
+        */
+
+
+
+        /*
+        if (a  2 ==% 0) {
+            //5 = 2*2 остаток 1; мы получили тем самым остаток от деления. НЕ ДЕЛЕНИЕ, а именно остаток.
+            System.out.println("Чётное");
+        }else{
+            System.out.println("нечётное");
+
+        };
+        */
+
     }
 }
