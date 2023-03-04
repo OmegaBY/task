63379c0494dbb9c7e91ed81886a0786ef257e4ee
Aliaksei Samborskiy <samborskiyalex@gmail.com>
2023-03-05 00:13:37 +0300

    00 - lesson - 3 -HW поел гавна
    Создать два класса, первый только для запуска второй класс радуга.
    В классе радуга, делаем возможность вывода цвета в зависимости от того какой цвет хотим запросить( вводим номер цвета получаем его строковый вариант).
    Так же радуга может выводить не только основные цвета но еще и полуцвета(желто-зеленый);
    Как будет выводиться продумать самостоятельно(но лучше если будет два аргумента заполнен один цвет(второй в этот момент пустой), два аргумента заполнено полуцвет)
    Название методов в зависимости от того что этот метод делает. Методы можно вызвать в друг друге.
    Думблирование вызовов метода возможно.
    вводимые цифры через константы
    все переменные, константы только на английском
    используем для решения задачи if и  switch
    каждый класс разносим по разным пакетам.

--- src/by/academy/Main.java
+++ src/by/academy/Main.java
@@ -1,8 +1,42 @@
 package by.academy;
 
+import by.academy.rainbow.Rainbow;
+
+import java.awt.*;
+
+import static javax.swing.UIManager.getColor;
+
 public class Main {
+    public static final int RED = 1;
+    public static final int ORANGE = 2;
+    public static final int YELLOW = 3;
+    public static final int GREEN = 4;
+    public static final int BLUE = 5;
+    public static final int INDIGO = 6;
+    public static final int VIOLET = 7;
 
     public static void main(String[] args) {
         System.out.println("Home Work (lesson 3)");
+        int color = BLUE;
+        int color2 = INDIGO;
+        Rainbow rainbow = new Rainbow();
+
+        int a ;
+
+        if (color > 0 && color < 8) {
+
+            rainbow.=color
+            System.out.println("выбран цвет " + );
+            if (color2 > 0 && color2 < 8) {
+                a = color2;
+                System.out.println (getColor(a));
+            }
+
+        } else {
+            System.out.println("Первое значение цвета не выбрано.Выберете цвет");
+        }
+
+
+
     }
 }
---
+++ src/by/academy/rainbow/Rainbow.java
@@ -0,0 +1,33 @@
+package by.academy.rainbow;
+
+public class Rainbow {
+
+    public static String getClr(int a){
+      switch (a) {
+          case 1:
+              return  "red";
+
+          case 2:
+              return "orange";
+
+          case 3:
+              return "yellow";
+
+          case 4:
+              return "green";
+
+          case 5:
+              return "blue";
+
+          case 6:
+              return "indigo";
+
+          case 7:
+              return "violet";
+
+          default:
+             break;
+      }
+    }
+
+}
